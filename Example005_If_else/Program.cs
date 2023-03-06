92d776ab9cbc27a81ac31c844dc39eb4e290f7e0
Sergey Atepalikhin <vinyl51@ya.ru>
2023-03-06 20:25:36 +0300

    If_Else

---
+++ Example005_If_else/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "дина")
+{
+    Console.WriteLine("А я тя люблю!");
+}
+else
+{
+    Console.Write("Привета!");
+    Console.WriteLine(username);
+}
