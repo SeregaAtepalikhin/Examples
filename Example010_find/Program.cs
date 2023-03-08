f7082a589552266545a6dc84c266fa2d6a179e2d
Sergey Atepalikhin <vinyl51@ya.ru>
2023-03-08 23:20:53 +0300

    primers

---
+++ Example010_find/Program.cs
@@ -0,0 +1,18 @@
+﻿int[] array = { 1, 12, 22, 23, 66, 33, 44, 55, 66, 88, 77 };
+
+int n = array.Length;
+int find = 66;
+
+int index = 0;
+
+while (index < n)
+{
+
+  if (array[index] == find)
+  {
+    Console.WriteLine(index);
+    break;
+  }
+  //index = index +1;
+  index++;
+}
